400bb9e2b5217e6097979bf105093b10e1b40a16
Kyle Taylor <kyle.a.taylor@gmail.com>
2017-09-21 13:42:43 -0600

    house keeping

--- build_tiger_roads_in_target_shapefile.sh
+++ build_tiger_roads_in_target_shapefile.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+#
+# this will download county-level TIGER 'all roads' data for the entire US
+# and unpack/merge shapefiles into a single target shapefile, accounting for
+# a user-specified boundary extent (hardcoded).
+#
+
 if [ $# -eq 0 ]; then
   exit "please specify a filename argument for our output"
 fi
+if [ -d "src" ]; then
+  mkdir src
+fi
 
-old_dir=$PWD
 shopt -s nullglob # for our files array
 
-if [[ `find src/ | grep ".zip$" | wc -l` -eq 0 ]]; then
+if [[ `find $PWD | grep ".zip$" | wc -l` -eq 0 ]]; then
   print " -- downloading:"
-  wget -r --no-parent --reject "index.html*" ftp://ftp2.census.gov/geo/tiger/TIGER2016/ROADS/
-elif [[ `find src/ | grep ".shp$" | wc -l` -eq 0 ]]; then
-  cd src/ftp2.census.gov/geo/tiger/TIGER2016/ROADS/
+  wget -r --no-parent -nH --cut-dirs=1 --reject "index.html*" ftp://ftp2.census.gov/geo/tiger/TIGER2016/ROADS/
+fi
+if [[ `find $PWD | grep ".shp$" | wc -l` -eq 0 ]]; then
   print " -- unpacking"
   for f in `ls -1 *.zip`; do
     7za x $f
   done
   files=(*.shp)
 else
-  cd src/ftp2.census.gov/geo/tiger/TIGER2016/ROADS/
   files=(*.shp)
 fi
 
@@ -26,8 +33,10 @@ echo " -- merging and subsetting input data:"
 
 ogr2ogr -f "ESRI Shapefile" $1 "${files[0]}"
 
-for i in "${files[@]:1}"; do # slice to end of array (skipping 0)
-  ogr2ogr -f "ESRI Shapefile" -clipsrc /global_workspace/ebird_number_crunching/ebird_reference_dataset_erd/erd/central_flyway.shp -append -update $1 "${files[$i]}"
+for fn in "${files[@]:1}"; do # slice to end of array (skipping 0)
+  ogr2ogr -f "ESRI Shapefile" -clipsrc "/global_workspace/ebird_number_crunching/ebird_reference_dataset_erd/erd/central_flyway.shp" -append -update $1 $fn
 done
 
-mv "${1/.shp/.*}" "$old_dir/"
+ mv "${1/.shp/.*}" "../"
+
+echo " -- done"
