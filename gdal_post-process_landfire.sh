725861d981c5a333557e4483794e16f1fb2712a1
Kyle Taylor <ktaylora@users.noreply.github.com>
2017-09-28 09:30:15 -0600

    Update gdal_post-process_landfire.sh

--- gdal_post-process_landfire.sh
+++ gdal_post-process_landfire.sh
@@ -9,7 +9,7 @@ if echo $in | grep -q "evh"; then
     gdal_calc.py -A shrub_height.tif --calc="(A>=0)*(A<=3250)*A" --outfile=shrub_height.tif
   gdal_calc.py -A $1 --outfile=tree_height.tif --calc="0*(A<108)+(0*(A>111))+((A==108)*25)+((A==109)*750)+((A==110)*1750)+((A==111)*3750)" --type="Int16"
     gdal_calc.py -A tree_height.tif --calc="(A>=0)*(A<=3750)*A" --outfile=tree_height.tif
-elif echo $in | grep -q "evh"; then
+elif echo $in | grep -q "evc"; then
   rm -rf grass_perc_cover* shrub_perc_cover* tree_perc_cover*
   gdal_calc.py -A $1 --outfile=grass_perc_cover.tif --calc="(((A>=121))*((A<=129)))*(((A-120)*10)+5)" --type="Int16"
     gdal_calc.py -A grass_perc_cover.tif --calc="(A>=0)*(A<=100)*A" --outfile=grass_perc_cover.tif
