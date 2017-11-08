3a9fff9de1d0ceb4a44163a8711b39ffeaf0fd35
Kyle Taylor <kyle.a.taylor@gmail.com>
2017-11-08 11:37:22 -0700

    house keeping

--- fetch_and_process_faa_wind_digital_obstruction_data.R
+++ fetch_and_process_faa_wind_digital_obstruction_data.R
@@ -1,57 +1,82 @@
-require(rgdal)
-require(raster)
+na.omit.list <- function(y) { return(y[!sapply(y, function(x) all(is.na(x)))]) }
 
 # determine which FAA file from index.html is the most recent and download it.
-f <- readLines("to_fetch.dat")
-
-toFetch <- unlist(strsplit(f,split="DOF_"))
+if(!file.exists("faa_obs.zip")){
+  f <- readLines("to_fetch.dat")
+  toFetch <- unlist(strsplit(f,split="DOF_"))
   toFetch <- toFetch[grepl(toFetch,pattern="zip")]
-    toFetch <- unlist(strsplit(toFetch,split="[.]"))
-      toFetch <- as.numeric(toFetch[!grepl(toFetch,pattern="zip")])
+  toFetch <- unlist(strsplit(toFetch,split="[.]"))
+  toFetch <- as.numeric(toFetch[!grepl(toFetch,pattern="zip")])
+  download.file(f[which(toFetch == max(toFetch))],destfile="faa_obs.zip")
+}
 
-download.file(f[which(toFetch == max(toFetch))],destfile="faa_obs.zip")
-
-cat(" -- decompressing ...")
-unzip("faa_obs.zip")
+cat(" -- decompressing\n")
+success <- suppressMessages(unzip("faa_obs.zip"))
+if ( !length(success) ){
+  stop("encountered a problem decompressing faa_obs.zip file!")
+}
 
 toDelete <- list.files(pattern="Dat$|dat$")
   toDelete <- toDelete[!grepl(toDelete,pattern="DOF")]
-    file.remove(toDelete)
+    suppressMessages(unlink(toDelete))
 
 f <- readLines("DOF.DAT",skip=4)
 
 cat(" -- parsing FAA obstructions for windmills: ")
-turbines <- data.frame()
-for(i in 1:length(f)){
-  j <- gsub(unlist((strsplit(f[i],split=" "))),pattern="N|W",replacement="")
-  # is this obstruction a windmill?
-  if(sum(grepl(j,pattern="MILL"))>0){
-    degMinSecN <- vector()
-    degMinSecW <- vector()
-          year <- vector()
-           ors <- j[1]
-    # because the exact field is never precisely known, we have to slowly iterate over all
-    # fields to find the coordinates of the turbine
-    for(k in 2:length(j)){
-      if(!is.na(as.numeric(j[k]))){
-        if(length(degMinSecN)<3){
-          degMinSecN[length(degMinSecN)+1] <- as.numeric(j[k])
-        } else if(length(degMinSecW)<3){
-          degMinSecW[length(degMinSecW)+1] <- as.numeric(j[k])
-        } else if(nchar(j[k])==7){
-          year <- as.numeric(substr(j[k],1,4))
+
+turbines <- lapply(
+  X=1:length(f),
+  FUN=function(x){
+    j <- gsub(unlist((strsplit(f[x],split=" "))),pattern="N|W",replacement="")
+    # is this obstruction a windmill?
+    if(sum(grepl(j,pattern="MILL"))>0){
+      degMinSecN <- vector()
+      degMinSecW <- vector()
+            year <- vector()
+             ors <- j[1]
+      # because the exact field is never precisely known, we have to slowly iterate over all
+      # fields to find the coordinates of the turbine
+      for(k in 2:length(j)){
+        if(!is.na(as.numeric(j[k]))){
+          if(length(degMinSecN)<3){
+            degMinSecN[length(degMinSecN)+1] <- as.numeric(j[k])
+          } else if(length(degMinSecW)<3){
+            degMinSecW[length(degMinSecW)+1] <- as.numeric(j[k])
+          } else if(nchar(j[k])==7){
+            year <- as.numeric(substr(j[k],1,4))
+          }
         }
       }
+      # now convert everything to decimal degrees and merge with our turbines data.frame
+      lat <- degMinSecN[1] + degMinSecN[2]/60 + degMinSecN[3]/3600
+      lon <- -1*(degMinSecW[1] + degMinSecW[2]/60 + degMinSecW[3]/3600)
+      cat("+")
+      return(data.frame(ors=ors,year=year,latitude=lat,longitude=lon))
+    } else {
+      return(NA)
     }
-    # now convert everything to decimal degrees and merge with our turbines data.frame
-    lat <- degMinSecN[1] + degMinSecN[2]/60 + degMinSecN[3]/3600
-    lon <- -1*(degMinSecW[1] + degMinSecW[2]/60 + degMinSecW[3]/3600)
-    turbines <- rbind(turbines,data.frame(ors=ors,year=year,latitude=lat,longitude=lon))
-    cat("+")
-  }
-}; cat("\n");
+  })  
 
-pts <- SpatialPointsDataFrame(coords=data.frame(x=turbines$longitude,y=turbines$latitude), data=data.frame(ors=turbines$ors,year=turbines$year))
-  projection(pts) <- projection("+init=epsg:4326")
+turbines <- plyr::ldply(na.omit.list(turbines), rbind)
 
-writeOGR(pts, ".", paste("wind_turbines_",toFetch[which(toFetch == max(toFetch))],sep=""), driver="ESRI Shapefile", overwrite=T)
+pts <- sp::SpatialPointsDataFrame(
+    coords=data.frame(x=turbines$longitude,y=turbines$latitude), 
+    data=data.frame(ors=turbines$ors,year=turbines$year)
+  )
+  
+raster::projection(pts) <- raster::projection("+init=epsg:4326")
+
+rgdal::writeOGR(
+    pts, 
+    ".", 
+    paste(
+      "wind_turbines_",
+      tolower(gsub(format(Sys.time(), "%b %d %Y"), pattern=" ", replacement="_")),
+      sep=""
+    ), 
+    driver="ESRI Shapefile", 
+    overwrite=T, 
+    verbose=F
+  )
+# clean-up
+unlink(c("DOF_README.pdf", "index.html", "faa_obs.zip"))
--- fetch_and_process_faa_wind_digital_obstruction_data.sh
+++ fetch_and_process_faa_wind_digital_obstruction_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+BASH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 rm -rf index.html;
 rm -rf to_fetch.dat;
 
-wget https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dof/
+echo " "
+wget –quiet https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dof/ >/dev/null 2>&1
+
 cat index.html | grep zip | grep "a href" | awk '{ print $2 }' | cut -d '"' -f2 | awk '{ print "https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dof/" $1 }' >> to_fetch.dat
 
-R --no-save --vanilla --slave < fetch_and_process_faa_wind_digital_obstruction_data.R
+Rscript $BASH_DIR"/fetch_and_process_faa_wind_digital_obstruction_data.R"
 
 rm -rf *DAT *EXE *exe
